0fee04df760668f53ecfe676c94cfaac04036013
imran hossain <imranme.global@gmail.com>
2026-05-05 00:44:09 +0600

    objLiteral&optional

---
+++ src/objLiteral&OptionalType.ts
@@ -0,0 +1,14 @@
+// Object types
+
+let user: { name: string, age: number } = {
+    name: 'imran',
+    age: 22
+}
+
+// literal type 
+let status: 'success' = 'success';
+
+// Optional Type 
+let users: { name: string; age?: number } = {
+    name: "Imran"
+};
