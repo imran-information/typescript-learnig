e148174f557300f5b0c70a74807ed3a6dc7b60cf
imran hossain <imranme.global@gmail.com>
2026-05-05 00:16:02 +0600

    non-primitive types explore

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,33 @@
+let usre = {
+    name: "imran",
+    age: 22
+}
+
+let numbers = [1, 2, 3, 4]
+
+
+function great() {
+    console.log("hello ");
+
+}
+
+
+let mePerson: [string, number] = ["imran", 21]
+
+enum Role {
+    Admin,
+    User
+}
+
+interface User {
+    name: string;
+    age: number;
+}
+
+class Person {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
--- src/primitive.ts
+++ src/primitive.ts
@@ -4,6 +4,8 @@ const age: number = 20;
 
 const isMarred: boolean = false;
 
+const emty: null = null;
+
 let any = undefined;
 
 any = 'cheking'
