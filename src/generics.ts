39e2beab902ac36d6167f10b4580113889c1d376
imran hossain <imranme.global@gmail.com>
2026-05-05 05:42:49 +0600

    generics explore

---
+++ src/generics.ts
@@ -0,0 +1,13 @@
+type GenericsType<T> = T
+
+const myName: GenericsType<string> = 'imran'
+
+const age: GenericsType<number> = 21;
+
+const marrid: GenericsType<boolean> = false;
+
+const myRollNumber: GenericsType<Array<number>> = [10, 20, 30, 40, 50];
+
+
+
+
